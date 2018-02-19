8425e4cfe44ad00e39da286154fff92900950884
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-02-19 10:02:15 -0800

    add js insertion sort tests

--- insertion-sort.ts
+++ insertion-sort.ts
@@ -5,7 +5,7 @@ const numericalIsLessThan: Compare<number> = (a, b) => a < b;
 /**
  * In place N^2 sort implementation with arrays
  */
-const insertionSort = <T extends any>(items: T[], isLessThan: Compare<T> = numericalIsLessThan): T[] => {
+const insertionSort = <T>(items: T[], isLessThan: Compare<T> = numericalIsLessThan): T[] => {
     let i: number;
     for (i = 0; i < items.length; i++) {
         let j: number = i;
@@ -25,4 +25,10 @@ const swap = (items: any[], i: number, j: number): void => {
     items[i] = items[j];
     items[j] = iItem;
 };
+
+/**
+ * TEST
+ */
+insertionSort<number>([1,4,3,4,3,4,4,2,4,8,1,4,8,9,1]); // [ 1, 1, 1, 2, 3, 3, 4, 4, 4, 4, 4, 4, 8, 8, 9 ]
+insertionSort<string>(['a', 'y', 'c', 'd']); // [ 'a', 'c', 'd', 'y' ]
 
